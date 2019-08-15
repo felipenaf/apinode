f53421cc1de9694481cc24cdf2bd8be2511da4f8
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2019-08-14 21:27:06 -0300

    Teste com parametro na url

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+app.get('/cliente/:id', function (req, res) {
+  if(req.params['id'] == 1){
+    res.send({
+      nome: "Felipe",
+      cargo: "Dev",
+      tel: "11 95420-6364"
+    })
+  }else{
+    res.send(req.params)
+  }
 })
  
 app.listen(3000)
