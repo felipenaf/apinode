696a4556181b945947e3e52d23e34f31689ea514
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2019-08-17 10:19:00 -0300

    class connection

--- db/Connection.class.js
+++ db/Connection.class.js
@@ -1,20 +1,27 @@
 const mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "felipe",
-    password: "123"
-});
+class Connection {
+    constructor() {
+        this.con = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "123"
+        });
+    }
+}
 
-con.connect(function(err) {
+var conn = new Connection()
+
+conn.con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("select * from cf_prod.grupo", function (err, result) {
-        if (err) throw err;
-        let i = 1
-        result.forEach(element => {
-            console.log(i + ": " + JSON.stringify(element));
-            i++
-        });
+});
+
+conn.con.query("select * from cf_prod.grupo", function (err, result) {
+    if (err) throw err;
+    let i = 1
+    result.forEach(element => {
+        console.log(i + ": " + JSON.stringify(element));
+        i++
     });
 });
